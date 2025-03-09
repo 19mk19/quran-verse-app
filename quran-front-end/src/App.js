904184a0880023c525e51223cc00413abc457b5a
MK <maazanwerkhan@gmail.com>
2025-03-09 19:54:06 +0300

    remove vercel analytics

--- quran-front-end/src/App.js
+++ quran-front-end/src/App.js
@@ -11,7 +11,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
